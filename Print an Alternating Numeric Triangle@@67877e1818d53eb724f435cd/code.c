d00a2570b5bd3880f7433aef5fc8f0c7b651f0f8
Dhruvbansal21 <bansaldhruv03555@gmail.com>
2025-03-29 15:56:56 +0530

    Solution to Print an Alternating Numeric Triangle

--- Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
+++ Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
@@ -1,26 +1,15 @@
-#include <stdio.h>  
+#include <stdio.h>
+int main(){
+    int i,j,a,c;
+    scanf("%d",&a);
 
-int main() {  
-    int N, num = 1; // Initialize N and a counter num  
+    for(i=0;i<a;i++){
+     
+        for(j=0;j<=i;j++){
+            printf("%c ",c);
+            c++;
+        }printf("\n");
+    }
 
-    // Prompt user for input  
-    printf("Enter the number of rows: ");  
-    scanf("%d", &N); // Read the number of rows  
-    
-    // Loop through each row  
-    for (int i = 1; i <= N; i++) {  
-        // Print leading spaces for pyramid shape  
-        for (int j = i; j < N; j++) {  
-            printf(" "); // Add space before numbers  
-        }  
-
-        // Print numbers in each row  
-        for (int j = 1; j <= i; j++) {  
-            printf("%d ", num++); // Print number and increment  
-        }  
-
-        printf("\n"); // Move to the next row  
-    }  
-
-    return 0;  
+ return 0;
 }
